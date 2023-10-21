2636f3630cd2afe5bc3d3a23f5f07733e990430c
imaxwellperry <maxwell-perry.i@northeastern.edu>
2023-10-20 23:26:40 -0400

    another fix

--- src/Labs/a3/index.js
+++ src/Labs/a3/index.js
@@ -1,11 +1,23 @@
+import Classes from "./Classes";
+import Styles from "./Styles";
 import JavaScript from "./JavaScript";
+import PathParameters from "./PathParameters";
+import ConditionalOutput from "./ConditionalOutput";
+import TodoItem from "./todos/TodoItem";
+import TodoList from "./todos/TodoList";
 
 function Assignment3() {
-    return (
-      <div className="container">
-        <h1>Assignment 3</h1>
-        <JavaScript/>
-      </div>
-    );
-  }
-  export default Assignment3;
+ return (
+   <div>
+     <h1>Assignment 3</h1>
+     <ConditionalOutput/>
+     <Styles/>
+     <Classes/>
+     <PathParameters/>
+     <JavaScript/>
+     <TodoItem/>
+     <TodoList/>
+   </div>
+ );
+}
+export default Assignment3;
