e53ab5bf2d24740c3c442c1171fb3a6d0478d208
imaxwellperry <maxwell-perry.i@northeastern.edu>
2023-12-12 17:09:26 -0500

    --empty-message

--- src/index.js
+++ src/index.js
@@ -7,6 +7,6 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1>Hello</h1>
+    <App />
   </React.StrictMode>
 ); 
