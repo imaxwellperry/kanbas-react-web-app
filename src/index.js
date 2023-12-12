73b6214743bc662d75eb012b481dcabc3daef167
imaxwellperry <maxwell-perry.i@northeastern.edu>
2023-12-12 18:28:46 -0500

    --empty-message

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 const root = ReactDOM.createRoot(document.getElementById('pocket'));
 root.render(
- <React.StrictMode>
    <App />
- </React.StrictMode>
 );
 
