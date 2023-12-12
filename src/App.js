48bd3a73c9ce661c8e099a12be2bbf39b85bd513
imaxwellperry <maxwell-perry.i@northeastern.edu>
2023-12-12 18:20:33 -0500

    --empty-message

--- src/App.js
+++ src/App.js
@@ -4,17 +4,8 @@ import Kanbas from "./Kanbas";
 import {HashRouter} from "react-router-dom";
 import {Routes, Route, Navigate} from "react-router";
 function App() {
-   return (
-       <HashRouter>
-           <div>
-           <Routes>
-           <Route path="/"         element={<Navigate to="/Labs"/>}/>
-          <Route path="/hello"    element={<HelloWorld/>}/>
-          <Route path="/Labs/*"   element={<Labs/>}/>
-          <Route path="/Kanbas/*" element={<Kanbas/>}/>
-        </Routes>
-           </div>
-       </HashRouter>
-   );
+  return (
+      <h1>Hello World!</h1>
+  );
 }
 export default App;
