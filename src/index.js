eb50e9caa3edff00b65169fe753cff20606d05a9
imaxwellperry <maxwell-perry.i@northeastern.edu>
2023-12-12 18:31:02 -0500

    --empty-message

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
-const root = ReactDOM.createRoot(document.getElementById('pocket'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-   <App />
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
