789bd627ac348af9dfe6ab74caa30eca7007add7
imaxwellperry <maxwell-perry.i@northeastern.edu>
2023-12-13 18:22:18 -0500

    --empty-message

--- src/Kanbas/index.js
+++ src/Kanbas/index.js
@@ -1,12 +1,22 @@
-import {Link} from "react-router-dom";
+import KanbasNavigation from "./KanbasNavigation";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Dashboard from "./Dashboard";
+import Courses from "./Courses";
+
+
 function Kanbas() {
- return(
-  <div>
-   <Link to="/hello">Hello</Link> |
-   <Link to="/Labs/a3">A3</Link> |
-   <Link to="/Kanbas">Kanbas</Link>
-   <h1>Kanbas</h1>
-  </div>
- )
+  return (
+    <div className="d-flex">
+      <KanbasNavigation />
+      <div>
+        <Routes>
+          <Route path="/" element={<Navigate to="Dashboard" />} />
+          <Route path="Account" element={<h1>Account</h1>} />
+          <Route path="Dashboard" element={<Dashboard />} />
+          <Route path="Courses/:courseId/*" element={<Courses />} />
+        </Routes>
+      </div>
+    </div>
+  );
 }
-export default Kanbas
+export default Kanbas;
