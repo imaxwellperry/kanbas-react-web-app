8a77bddf3131ae5f14a785f1a6fd218bb229a1a3
imaxwellperry <maxwell-perry.i@northeastern.edu>
2023-12-12 19:39:59 -0500

    --empty-message

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import Labs from "./Labs";
-import HelloWorld from "./Labs/a3/HelloWorld";
-import Kanbas from "./Kanbas";
-import {HashRouter} from "react-router-dom";
-import {Routes, Route, Navigate} from "react-router";
-function App() {
-  return (
-      <h1>Hello World!</h1>
-  );
-}
-export default App;
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+ <React.StrictMode>
+   <App />
+ </React.StrictMode>
+);
