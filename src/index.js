4c03605da15040b9b1e6b95fe1ffbee0addafe99
imaxwellperry <maxwell-perry.i@northeastern.edu>
2023-12-14 10:44:03 -0500

    --empty-message

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
-import React from 'react';
+import React, { createElement } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const elem = createElement(<h1>Hi</h1>);
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-); 
+root.render(elem); 
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
